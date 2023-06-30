d5fb7116ef2644a4d6c362d23171c14e657a886d
Rusik-hub <pluxindyu@mail.ru>
2023-06-30 15:39:34 +0500

    adgdxfhdxfghnfgngfb

--- Assets/Scripts/SignalizationController.cs
+++ Assets/Scripts/SignalizationController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class SignalizationController : MonoBehaviour
 {
     [SerializeField] private TriggerChecker _signalizationZone;
 
     private AudioSource _audio;
+    private float _volumeMoveScale = 0.001f;
 
     private void Start()
     {
-        if (GetComponent<AudioSource>())
-            _audio = GetComponent<AudioSource>();
+        _audio = GetComponent<AudioSource>();
+        _audio.volume = 0;
     }
 
     private void OnEnable()
@@ -29,22 +32,24 @@ public class SignalizationController : MonoBehaviour
 
     private void EnableSignalization()
     {
-        Debug.Log("Audio on");
         _audio.Play();
         StartCoroutine(MoveVolumeToTarget(1f));
     }
 
     private void DisableSignalization()
     {
-        Debug.Log("Audio off");
         StartCoroutine(MoveVolumeToTarget(0f));
         _audio.Pause();
     }
 
     private IEnumerator MoveVolumeToTarget(float target)
     {
-        _audio.volume = Mathf.MoveTowards(_audio.volume, target, 0.01f);
+        while (_audio.volume != target)
+        {
+            _audio.volume = Mathf.MoveTowards(_audio.volume, target, _volumeMoveScale);
 
-        yield return null;
+            yield return null;
+        }
+
     }
 }
